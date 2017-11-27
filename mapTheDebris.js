9cde75263a1e8631b02082817f862f8ad2768c0a
Sghir Nouredine <sghir@imist.ma>
2017-11-27 15:07:06 +0000

    working copy of mapTheDebris

--- mapTheDebris.js
+++ mapTheDebris.js
@@ -1,6 +1,16 @@
 function orbitalPeriod(arr) {
     var GM = 398600.4418;
     var earthRadius = 6367.4447;
+    var PI = 3.1415;
+    var orbits = arr.map(function(elem){
+    var radius = elem.avgAlt +  earthRadius;  
+    console.log(radius);  
+    var  orbitalPeriod =Math.round( 2 *  Math.PI * Math.sqrt(Math.pow(radius, 3)/GM));
+    elem.orbitalPeriod =orbitalPeriod;
+    delete(elem.avgAlt);
+    return elem;
+    });
+    
     return arr;
   }
   
